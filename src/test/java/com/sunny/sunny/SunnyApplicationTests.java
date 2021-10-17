03c0e8f678cc21dfdbb8577891393bdbfee3d42c
luiz barros <luizbarro82@gmail.com>
2021-10-17 18:41:19 -0300

    Sunny

---
+++ src/test/java/com/sunny/sunny/SunnyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sunny.sunny;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SunnyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
